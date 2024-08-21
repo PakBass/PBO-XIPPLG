2dac773787f55006a9d3b71fe39663c8c302a0c4
PakBass <andhibasuki@gmail.com>
2024-08-21 11:16:52 +0800

    Menambahkan materi if

---
+++ latihan_if/lat_1.java
@@ -0,0 +1,12 @@
+package latihan_if;
+
+public class lat_1 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 12;
+
+        if (a > b) {
+            System.out.println("Variabel a lebih besar");
+        }
+    }
+}
---
+++ latihan_if/lat_2.java
@@ -0,0 +1,14 @@
+package latihan_if;
+
+public class lat_2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+            //0     OR    1
+        if ((a > b) || (a < b)) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+    }
+}
