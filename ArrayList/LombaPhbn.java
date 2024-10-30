b664c7116929453df30c5020efc1c08cb40c162a
PakBass <andhibasuki@gmail.com>
2024-10-30 09:52:20 +0800

    menambah fitur ubah data - 30 Oktober 2024

--- ArrayList/LombaPhbn.java
+++ ArrayList/LombaPhbn.java
@@ -12,6 +12,7 @@ class Siswa {
         this.kelas = kelas;
     }
 
+    //getter
     public String getNama(){
         return nama;
     }
@@ -20,6 +21,17 @@ class Siswa {
     }
     public String getKelas(){
         return kelas;
+    }
+
+    //setter
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public void setMataLomba(String mataLomba){
+        this.mataLomba = mataLomba;
+    }
+    public void setKelas(String kelas){
+        this.kelas = kelas;
     }
 }
 
@@ -32,12 +44,41 @@ public class LombaPhbn {
     }
 
     public void tampilkanData(){
-        System.out.println("Daftar Peserta");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("Daftar Peserta PHBN 2024");
         System.out.println("---------------------------------------");
         System.out.println("| Nama Siswa      | Mata Lomba | Kelas  |");
         System.out.println("---------------------------------------");
         for (Siswa siswa : listSiswa) {
             System.out.printf("| %-15s | %-10s | %-9s |\n", siswa.getNama(), siswa.getMataLomba(), siswa.getKelas());
+        }
+    }
+    public void updateData(String nama){
+        Scanner input = new Scanner(System.in);
+        boolean cariData = false;
+
+        for (Siswa siswa : listSiswa) {
+            if (siswa.getNama().equalsIgnoreCase(nama)) {
+                System.out.print("Masukkan nama baru (kosongkan jika tidak ingin mengubah): ");
+                String namaBaru = input.nextLine();
+                if (!namaBaru.isEmpty()) {
+                    siswa.setNama(namaBaru);
+                }
+                System.out.print("Masukkan mata lomba baru (kosongkan jika tidak ingin mengubah): ");
+                String mataLombaBaru = input.nextLine();
+                if (!mataLombaBaru.isEmpty()) {
+                    siswa.setMataLomba(mataLombaBaru);
+                }
+                System.out.print("Masukkan kelas baru (kosongkan jika tidak ingin mengubah): ");
+                String kelasBaru = input.nextLine();
+                if (!kelasBaru.isEmpty()) {
+                    siswa.setKelas(kelasBaru);
+                }
+                System.out.println("Data siswa berhasil di update!");
+                cariData = true;
+                break;
+            }
         }
     }
     public static void main(String[] args) {
@@ -46,14 +87,15 @@ public class LombaPhbn {
 
         int pilih;
         do{
-
             System.out.println();
             System.out.println("-------------------------");
             System.out.println("Pilihan Menu");
             System.out.println("-------------------------");
             System.out.println("1. Tambah data peserta");
             System.out.println("2. Tampilkan semua data");
-            System.out.println("3. Keluar program");
+            System.out.println("3. Ubah data");
+            System.out.println("4. Hapus data");
+            System.out.println("5. Keluar program");
             System.out.println("-------------------------");
             System.out.print("Masukkan pilihan anda: ");
             pilih = input.nextInt();
@@ -73,8 +115,13 @@ public class LombaPhbn {
                 case 2 :
                         lomba.tampilkanData();
                     break;
+                case 3 :
+                        System.out.print("Masukkan nama siswa yang  ingin di ubah: ");
+                        String namaUpdate = input.nextLine();
+                        lomba.updateData(namaUpdate);
+                        break;
             }
-        } while (pilih != 3);
+        } while (pilih != 5);
         System.out.println("Keluar program");
     }
 }
