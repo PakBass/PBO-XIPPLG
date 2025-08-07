7b6d3a3e3043bb2e06b1ba6642a35b9705e6e30b
PakBass <andhibasuki@gmail.com>
2025-08-07 08:57:07 +0800

    Menambah nama programer

--- 2025/PBO/Soal_for.java
+++ 2025/PBO/Soal_for.java
@@ -1,10 +1,12 @@
 public class Soal_for {
     public static void main(String[] args) {
+        String nama = "Mason";
         int menabung = 0;
         for (int ayu = 0; ayu < 30; ayu++) {
             menabung += 1000;
             System.out.println("Menabung hari ke- " + (ayu + 1) + " = " + "Rp " + menabung);
         }
-        System.out.print("Total tabungam ayu selama 30 hari = Rp " + menabung);
+        System.out.println("Total tabungam ayu selama 30 hari = Rp " + menabung);
+        System.out.println("Nama programer adalah " + nama);
     }
 }
